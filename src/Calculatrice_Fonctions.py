db7455f69ee491770d3f700cee970bf7b66c9306
Anthony Simond <pv22nbo@eduvaud.ch>
2024-11-15 12:15:37 +0100

    my first commit

---
+++ src/Calculatrice_Fonctions.py
@@ -0,0 +1,20 @@
+"""
+Name: Calculatrice_Fonctions.py
+Author: Anthony Simond
+Date:11.11.2024
+"""
+
+def calculatrice():
+    print("Donne moi un premier chiffre:")
+    print("Donner un opérateur:")
+    print("Donne moi un  deuxième chiffre:")
+
+def add(nb,nb2):
+    return nb+nb2
+def sub(nb,nb2):
+    return nb-nb2
+def mul(nb,nb2):
+    return nb*nb2
+def div(nb,nb2):
+    return nb/nb2
+
